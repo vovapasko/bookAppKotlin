198d738f70119e5e4442c8d8213e5f46b2099aa3
vovapasko <vovapasko1699@gmail.com>
2022-08-12 13:16:43 +0200

    chore: provided mocking data

---
+++ src/main/resources/db/migrations/repeatable/R__mock_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO books(title, author, is_read)
+VALUES ('The Pleasure of My Company', 'Steve Martin', false),
+       ('Love Letters to the Dead', 'Ava Dellaira', false),
+       ('Charlie Bone and the Time Twister', 'Jenny Nimmo', false)
+
