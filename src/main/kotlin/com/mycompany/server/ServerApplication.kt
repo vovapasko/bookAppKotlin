f50689205ebc13068d498cf6bb6c7c310f2221b7
vovapasko <vovapasko1699@gmail.com>
2022-09-06 18:52:13 +0200

    feat: resolved cors problem

--- src/main/kotlin/com/mycompany/server/ServerApplication.kt
+++ src/main/kotlin/com/mycompany/server/ServerApplication.kt
@@ -1,4 +1,4 @@
-package com.mycompany.server.src
+package com.mycompany.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/main/kotlin/com/mycompany/server/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/mycompany/server/configuration/CorsConfiguration.kt
@@ -1,24 +1,34 @@
 package com.mycompany.server.configuration;
 
+import org.springframework.web.filter.CorsFilter
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.http.HttpMethod
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class CorsConfiguration {
 
     @Bean
-    fun configCors(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedMethods("*")
-                    .allowedOriginPatterns("http://localhost:8080")
-                    .allowCredentials(true)
+    fun configCors(): CorsFilter {
+        val corsConfig = CorsConfiguration()
+            .applyPermitDefaultValues().apply {
+                allowedMethods = listOf(
+                    HttpMethod.HEAD.name,
+                    HttpMethod.GET.name,
+                    HttpMethod.POST.name,
+                    HttpMethod.PUT.name,
+                    HttpMethod.DELETE.name,
+                    HttpMethod.PATCH.name
+                )
+                allowedOrigins = listOf("http://localhost:8080")
+                allowCredentials = true
+                addExposedHeader("Content-Disposition")
             }
-
-
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", corsConfig)
         }
+        return CorsFilter(source)
     }
 }
